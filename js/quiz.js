4f3d82bb9cf82b2ba3529bddf3568019069ef9b7
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-12-17 07:15:16 -0500

    added quiz functionality

--- js/quiz.js
+++ js/quiz.js
@@ -106,3 +106,143 @@ var questionsArr = [
     answer: "3",
   },
 ];
+
+var indexNum = 0;
+var timer = 59;
+var correct = 0;
+var wrong = 0;
+var play = true;
+var highScores = [];
+
+// function to shuffle the array
+var shuffleArr = function (array) {
+  var ctr = array.length,
+    temp,
+    index;
+  while (ctr > 0) {
+    index = Math.floor(Math.random() * ctr);
+    ctr--;
+    temp = array[ctr];
+    array[ctr] = array[index];
+    array[index] = temp;
+  }
+  return array;
+};
+
+// function for the timer
+var countdown = function () {
+  var timeInterval = setInterval(function () {
+    if (timer > 0 && play === true) {
+      timerEl.innerText = timer;
+      timer--;
+    } else {
+      timerEl.innerText = timer;
+      timerEl.setAttribute("style", "color: red;");
+      clearInterval(timeInterval);
+      endQuiz();
+    }
+  }, 1000);
+};
+
+// function to start timer and start quiz
+var startQuiz = function () {
+  // shuffle array
+  shuffleArr(questionsArr);
+  //start timer
+  countdown();
+  // style quiz display section elements
+  questionEl.setAttribute("style", "text-align: left;");
+  messageEl.setAttribute("style", "margin-left: 25px; width: fit-content;");
+  // remove start button
+  startBtn.remove();
+
+  runQuiz();
+};
+
+// function to start time and begin quiz
+var runQuiz = function () {
+  // if the indexNum is greater than length of the questionsArr, end quiz
+  if (indexNum === questionsArr.length) {
+    play = false;
+  } else {
+    // set variables from object in questionsArr index
+    var question = questionsArr[indexNum].question;
+    var options = questionsArr[indexNum].options;
+    answer = questionsArr[indexNum].answer;
+    // change h1 to the question
+    questionEl.textContent = question;
+    // remove div contents
+    messageEl.textContent = "";
+    // for each of the 4 options, create a button and append it to the now empty div
+    for (var i = 0; i < options.length; i++) {
+      var btnEl = document.createElement("button");
+      btnEl.className = "btn guess-list";
+      btnEl.setAttribute("btn-id", [i + 1]);
+      btnEl.textContent = `${[i + 1]}. ${options[i]}`;
+      messageEl.appendChild(btnEl);
+    }
+    // increment indexNum to use in next run through
+    indexNum++;
+    // at this point the application will wait for the user to click on the page before continuing
+  }
+};
+
+// function for when the user clicks on the screen
+var guessHandler = function (event) {
+  // on click, ensure a guess button was clicked
+  var targetEl = event.target;
+  if (targetEl.matches(".guess-list")) {
+    // get button id of guess, pass to guess compare function
+    var guessId = targetEl.getAttribute("btn-id");
+    guessCompare(guessId);
+  }
+};
+
+// function to compare the guess to the actual answer
+var guessCompare = function (guessId) {
+  // if guessed correct
+  if (guessId === answer) {
+    // add to timer, increase correct var, display correct, run next question
+    timer += 3;
+    correct++;
+    resultEl.innerText = "Correct!";
+    runQuiz();
+    // if guessed incorrect
+  } else {
+    // subtract from timer, increase wrong var, display wrong, run next question
+    timer -= 10;
+    wrong++;
+    resultEl.innerText = "Wrong!";
+    runQuiz();
+  }
+};
+
+// function to endQuiz when all questions answered or timer runs out
+var endQuiz = function () {
+  // ensure score can't be negative
+  if (timer < 0) {
+    timer = 0;
+    timerEl.innerText = timer;
+  }
+  // update DOM
+  questionEl.removeAttribute("style");
+  questionEl.textContent = "Let's see how you did!";
+  messageEl.innerHTML = `<div>You got ${correct} questions correct and ${wrong} questions wrong.</div><div>Your time score is: ${timer}.</div>`;
+  // create and append a form elements for submitting initials
+  var formEl = document.createElement("form");
+  formEl.setAttribute("id", "initials-form");
+  var inputEl = document.createElement("input");
+  inputEl.setAttribute("type", "text");
+  inputEl.setAttribute("name", "user-initials");
+  inputEl.className = "user-initials";
+  inputEl.setAttribute("placeholder", "Enter Your Initials");
+  formEl.appendChild(inputEl);
+  var submitEl = document.createElement("button");
+  submitEl.className = "btn";
+  submitEl.setAttribute("id", "save-initials");
+  submitEl.setAttribute("type", "submit");
+  submitEl.textContent = "Submit";
+  formEl.appendChild(submitEl);
+  // append the entire form to the messageEl
+  messageEl.appendChild(formEl);
+};
